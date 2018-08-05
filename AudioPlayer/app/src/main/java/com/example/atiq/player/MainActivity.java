313d46c28055bd44c29c0c8123f3f7448a028dc4
atiqahammed <atiqahammedshamim@gmail.com>
2018-08-05 23:17:04 +0600

    external_storage write file has been added

--- AudioPlayer/app/src/main/java/com/example/atiq/player/MainActivity.java
+++ AudioPlayer/app/src/main/java/com/example/atiq/player/MainActivity.java
@@ -96,6 +96,77 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+
+    ////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /////////////////////////////////////////////////////////
+
+    private static final int FILE_SELECT_CODE = 0;
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
     private void initiateComponent() {
         songList = (Button) findViewById(R.id.song_list_btn);
         playBtn = (Button) findViewById(R.id.playBtn);
